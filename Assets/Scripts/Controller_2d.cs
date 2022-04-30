c887dabaf580414cde111597c74648e4319bded5
Jansharp <steinmeister36@gmail.com>
2022-04-30 03:27:55 +0200

    Add partially implemented 2D player contorller
    Update scene
    Add TestGraph

---
+++ Assets/Scripts/Controller_2d.cs
@@ -0,0 +1,49 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+///cSpell:ignore chipz
+
+public class Controller_2d : UdonSharpBehaviour
+{
+    public Rigidbody2D characterRB;
+    public Animator characterAnim;
+    public UdonBehaviour chipz2DHP;
+    public float speed;
+    public float jumpImpulse;
+    private bool isScarChan;
+    [FieldChangeCallback(nameof(onHorizontalValueChange))]
+    private float horizontalValue;
+
+    private float onHorizontalValueChange
+    {
+        set
+        {
+            characterAnim.SetFloat("Speed", Mathf.Abs(value));
+            horizontalValue = value;
+        }
+    }
+
+    public void Update()
+    {
+        HandleDirection(KeyCode.RightArrow, 1f);
+        HandleDirection(KeyCode.LeftArrow, -1f);
+    }
+
+    public void HandleDirection(KeyCode keycode, float direction)
+    {
+        if (Input.GetKey(keycode))
+        {
+            characterRB.transform.rotation = new Quaternion(0, 0, 0, 0);
+            characterRB.velocity = new Vector2(speed * direction, characterRB.velocity.y);
+            SetProgramVariable(nameof(horizontalValue), characterRB.velocity.x);
+        }
+        if (Input.GetKeyUp(keycode))
+        {
+            characterRB.velocity = new Vector2(0, characterRB.velocity.y);
+            SetProgramVariable(nameof(horizontalValue), 0f);
+        }
+    }
+}
