afb7fcb7a6a43894481f23fa2a44ad215a0e8dab
Jansharp <steinmeister36@gmail.com>
2022-05-20 17:37:39 +0200

    Add item syncing logic

--- Assets/Scripts/JanItemSync.cs
+++ Assets/Scripts/JanItemSync.cs
@@ -3,17 +3,27 @@ using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
+using VRC.Udon.Common;
 
 public class JanItemSync : UdonSharpBehaviour
 {
     public UpdateManager updateManager;
     public VRC_Pickup pickup;
 
+    // debugging
     public GameObject bonePositionVisualization;
+    // private float lastTime;
 
     private HumanBodyBones attachedBone;
     private Vector3 attachedLocalOffset;
     private Quaternion attachedRotationOffset;
+
+    [UdonSynced]
+    private byte syncedSmallData;
+    [UdonSynced]
+    private Vector3 syncedPosition;
+    [UdonSynced]
+    private Quaternion syncedRotation;
 
     // state tracking to determine when the player and item held still for long enough to really determine the attached offset
     private const float ExpectedStillFrameCount = 3;
@@ -30,6 +40,9 @@ public class JanItemSync : UdonSharpBehaviour
     // for the update manager
     private int customUpdateInternalIndex;
 
+    // update manager related
+    private bool customUpdateIsRegistered;
+
     public override void OnPickup()
     {
         Debug.Assert(pickup.IsHeld, "Picked up but not held?!");
@@ -37,7 +50,6 @@ public class JanItemSync : UdonSharpBehaviour
         attachedBone = pickup.currentHand == VRC_Pickup.PickupHand.Left ? HumanBodyBones.LeftHand : HumanBodyBones.RightHand;
         // boneTransform = pickup.currentPlayer.GetBoneTransform(attachedBone); // not exposed
         // Debug.Assert(boneTransform != null, "Didn't get the bone's transform.");
-        updateManager.Register(this);
 
         prevBonePos = pickup.currentPlayer.GetBonePosition(attachedBone);
         prevBoneRotation = pickup.currentPlayer.GetBoneRotation(attachedBone);
@@ -52,19 +64,28 @@ public class JanItemSync : UdonSharpBehaviour
 
         attachedRotationOffset = Quaternion.Inverse(prevBoneRotation) * prevItemRotation;
 
-        // this.transform.RotateAround(bonePosition, Quaternion.FromToRotation(boneRotation.axis))
+        RegisterCustomUpdate();
+        SendChanges();
     }
 
     public override void OnDrop()
     {
-        updateManager.Deregister(this);
         stillFrameCount = 0;
         heldStillLongEnough = false;
+        bonePositionVisualization.transform.SetPositionAndRotation(this.transform.position, this.transform.rotation);
+        DeregisterCustomUpdate();
+        SendChanges();
     }
 
-    private float lastTime;
-
     public void CustomUpdate()
+    {
+        if (Networking.IsOwner(this.gameObject))
+            UpdateSender();
+        else
+            UpdateReceiver();
+    }
+
+    private void UpdateSender()
     {
         // fetch values
         var player = pickup.currentPlayer;
@@ -75,7 +96,6 @@ public class JanItemSync : UdonSharpBehaviour
         // move some transform to match the bone, because the TransformDirection methods require an instance of a Transform
         visualTransform.SetPositionAndRotation(bonePos, boneRotation);
 
-        // determine item and player stillness and update the local offset accordingly
         if (heldStillLongEnough)
         {
             // I think this part still has to make sure the offset is about right, but we'll see
@@ -92,6 +112,7 @@ public class JanItemSync : UdonSharpBehaviour
         }
         else
         {
+            // determine item and player stillness and update the local offset accordingly
             var itemPos = this.transform.position;
             var itemRotation = this.transform.rotation;
             if (PositionsAlmostEqual(prevItemPos, itemPos) && PositionsAlmostEqual(prevBonePos, bonePos))
@@ -103,6 +124,7 @@ public class JanItemSync : UdonSharpBehaviour
                     attachedLocalOffset = visualTransform.InverseTransformDirection(itemPos - bonePos);
                     attachedRotationOffset = Quaternion.Inverse(boneRotation) * itemRotation;
                     Debug.Log($"Held still long enough, setting local offset to {attachedLocalOffset} with length {attachedLocalOffset.magnitude}.");
+                    SendChanges();
                 }
             }
             else
@@ -124,6 +146,7 @@ public class JanItemSync : UdonSharpBehaviour
                     attachedRotationOffset = currentRotationOffset;
                     Debug.Log($"Updating local pickup position, changed by {magnitudeDiff}, set to {attachedLocalOffset}, length {attachedLocalOffset.magnitude}.");
                     Debug.Log($"Updating local rotation, changed by {angle} degrees around the axis {axis}.");
+                    SendChanges();
                 }
             }
             prevBonePos = bonePos;
@@ -142,5 +165,91 @@ public class JanItemSync : UdonSharpBehaviour
     private bool PositionsAlmostEqual(Vector3 one, Vector3 two)
     {
         return (one - two).magnitude <= MagnitudeTolerance;
+    }
+
+    private void UpdateReceiver()
+    {
+        // prevent this object from being moved by this logic if the local player is holding it
+        // the local player should now be the owner, but it might take a few frames for ownership to transfer
+        if (pickup.IsHeld)
+            return;
+
+        // fetch values
+        var player = pickup.currentPlayer;
+        var bonePos = player.GetBonePosition(attachedBone);
+        var boneRotation = player.GetBoneRotation(attachedBone);
+
+        // move some transform to match the bone, because the TransformDirection methods require an instance of a Transform
+        this.transform.SetPositionAndRotation(bonePos, boneRotation);
+        this.transform.SetPositionAndRotation(
+            bonePos + this.transform.TransformDirection(attachedLocalOffset),
+            boneRotation * attachedRotationOffset
+        );
+    }
+
+    private void SendChanges()
+    {
+        RequestSerialization();
+    }
+
+    public override void OnPreSerialization()
+    {
+        syncedSmallData = 0;
+        if (pickup.IsHeld)
+        {
+            syncedSmallData += 1;
+            syncedPosition = attachedLocalOffset;
+            syncedRotation = attachedRotationOffset;
+        }
+        else
+        {
+            syncedPosition = this.transform.position;
+            syncedRotation = this.transform.rotation;
+        }
+        if (attachedBone == HumanBodyBones.LeftHand)
+            syncedSmallData += 2;
+    }
+
+    public override void OnPostSerialization(SerializationResult result)
+    {
+        if (!result.success)
+        {
+            Debug.LogWarning($"Syncing request was dropped for {this.name}, trying again.");
+            SendChanges(); // TODO: somehow test if this kind of retry even works or if the serialization request got reset right afterwards
+        }
+    }
+
+    public override void OnDeserialization()
+    {
+        if ((syncedSmallData & 1) != 0) // is held?
+        {
+            attachedBone = (syncedSmallData & 2) != 0 ? HumanBodyBones.LeftHand : HumanBodyBones.RightHand;
+            attachedLocalOffset = syncedPosition;
+            attachedRotationOffset = syncedRotation;
+            RegisterCustomUpdate();
+        }
+        else // not held
+        {
+            this.transform.SetPositionAndRotation(syncedPosition, syncedRotation);
+            DeregisterCustomUpdate();
+        }
+    }
+
+    private void RegisterCustomUpdate()
+    {
+        if (!customUpdateIsRegistered)
+        {
+            updateManager.Register(this);
+            customUpdateIsRegistered = true;
+        }
+    }
+
+    private void DeregisterCustomUpdate()
+    {
+        if (customUpdateIsRegistered)
+        {
+            updateManager.Deregister(this);
+            customUpdateIsRegistered = false;
+        }
     }
 }
