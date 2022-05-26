2b3d480b8c80589ef6cc581770075f0e2244ce05
Jansharp <steinmeister36@gmail.com>
2022-05-27 00:48:34 +0200

    Add first partial implementation of object pools

---
+++ Assets/Scripts/SimpleObjectPoolSpawner.cs
@@ -0,0 +1,16 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class SimpleObjectPoolSpawner : UdonSharpBehaviour
+{
+    public SimpleObjectPool objectPool;
+
+    public override void Interact()
+    {
+        Networking.SetOwner(Networking.LocalPlayer, objectPool.gameObject);
+        objectPool.TryToSpawn();
+    }
+}
