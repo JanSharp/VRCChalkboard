eed5c3162f606731973ba42886f0f609c0795567
Jansharp <steinmeister36@gmail.com>
2022-05-30 20:16:20 +0200

    Update Hook Stuff

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -322,4 +322,13 @@ public class TestScript : UdonSharpBehaviour
     //     }
     // }
     // return;
+
+    public void Event18()
+    {
+        int layer = 524288;
+        Debug.Log(layer.ToString("x8"));
+        for (int i = 0; i < 32; i++)
+            if ((layer & (1 << i)) != 0)
+                Debug.Log($"{i}: {LayerMask.LayerToName(i)}");
+    }
 }
