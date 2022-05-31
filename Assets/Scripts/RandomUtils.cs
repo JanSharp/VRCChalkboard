189fe29a5244fec2eaaac9bdf8a44fb32b3f805c
Jansharp <steinmeister36@gmail.com>
2022-05-31 21:55:57 +0200

    Add utility to change materials

--- Assets/Scripts/RandomUtils.cs
+++ Assets/Scripts/RandomUtils.cs
@@ -7,6 +7,12 @@ public class RandomUtils : MonoBehaviour
 {
     public GameObject objectToSpawn;
     public string objectToSpawnName;
+
+    [Header("Material Switcher")]
+    [Tooltip("This will replace all materials on a mesh renderer where their shader name matches the given name.")]
+    public GameObject[] mainParents;
+    public string oldShaderName;
+    public Material newMaterial;
 }
 
 #if UNITY_EDITOR
@@ -15,11 +21,11 @@ public class RandomUtilsEditor : Editor
 {
     public override void OnInspectorGUI()
     {
+        var target = (RandomUtils)base.target;
         base.OnInspectorGUI();
         EditorGUILayout.Space();
         if (GUILayout.Button(new GUIContent("Spawn 1000 objects")))
         {
-            var target = (RandomUtils)base.target;
             for (int y = 0; y < 10; y++)
                 for (int x = 0; x < 100; x++)
                 {
@@ -32,6 +38,42 @@ public class RandomUtilsEditor : Editor
                         obj = Instantiate(target.objectToSpawn, position, rotation, target.transform);
                     obj.name = $"{(string.IsNullOrWhiteSpace(target.objectToSpawnName) ? target.objectToSpawn.name : target.objectToSpawnName)}_{x}_{y}";
                 }
+        }
+
+        if (GUILayout.Button(new GUIContent("Switch Materials")))
+        {
+            if (target.mainParents == null
+                || target.oldShaderName == null
+                || target.newMaterial == null)
+            {
+                Debug.LogError("In order to change materials you must provide the 'Main Parent', "
+                    + "the 'Old Shader Name' and the 'New Material' which should replace the old material. "
+                    + "This will replace all materials on a mesh renderer where their shader name matches the given name."
+                );
+            }
+            else
+                foreach (GameObject obj in target.mainParents)
+                    RecursivelyChangeMaterial(target, obj.transform);
+        }
+    }
+
+    private void RecursivelyChangeMaterial(RandomUtils utils, Transform transformToWalk)
+    {
+        foreach (Transform child in transformToWalk)
+            RecursivelyChangeMaterial(utils, child);
+
+        MeshRenderer renderer = transformToWalk.GetComponent<MeshRenderer>();
+        if (renderer != null)
+        {
+            Material[] materials = renderer.sharedMaterials;
+            for (int i = 0; i < materials.Length; i++)
+            {
+                var shader = materials[i].shader;
+                if (materials[i].shader.name == utils.oldShaderName)
+                    materials[i] = utils.newMaterial;
+                // FIXME: find a way to compare shared materials with a provided material
+            }
+            renderer.sharedMaterials = materials;
         }
     }
 }
