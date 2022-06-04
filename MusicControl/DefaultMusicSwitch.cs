c5f49d9a0fbd3eff0578b1d8afe69f0165c05c64
JanSharp <steinmeister36@gmail.com>
2022-06-04 04:46:40 +0200

    Add DefaultMusicSwitch script

---
+++ MusicControl/DefaultMusicSwitch.cs
@@ -0,0 +1,17 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class DefaultMusicSwitch : UdonSharpBehaviour
+{
+    public MusicManager musicManager;
+    public MusicDescriptor musicToSwitchTo;
+
+    public void Switch()
+    {
+        musicManager.DefaultMusic = musicToSwitchTo;
+    }
+}
