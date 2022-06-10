2ec5ee0bd7bd02fc853f2f128d37e5ee22b8ee57
JanSharp <steinmeister36@gmail.com>
2022-06-11 01:25:48 +0200

    Add basic VRCObjectPool spawner

---
+++ Unfinished/VRCObjectPoolSpawner.cs
@@ -0,0 +1,20 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace JanSharp
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class VRCObjectPoolSpawner : UdonSharpBehaviour
+    {
+        public VRC.SDK3.Components.VRCObjectPool pool;
+
+        public override void Interact() => Spawn();
+        public void Spawn()
+        {
+            Networking.SetOwner(Networking.LocalPlayer, pool.gameObject);
+            pool.TryToSpawn();
+        }
+    }
+}
