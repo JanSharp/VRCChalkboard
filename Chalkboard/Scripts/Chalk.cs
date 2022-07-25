7d6032c1c204b1f920e27e4a36392f45b47999e8
JanSharp <steinmeister36@gmail.com>
2022-07-26 01:51:03 +0200

    Add chalk and chalkboard scripts and basic drawing

---
+++ Chalkboard/Scripts/Chalk.cs
@@ -0,0 +1,71 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace JanSharp
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class Chalk : UdonSharpBehaviour
+    {
+        [SerializeField] private Transform aimPoint;
+        // 0: Default, 4: Water, 8: Interactive, 11: Environment
+        [SerializeField] private LayerMask layerMask = (1 << 0) | (1 << 4) | (1 << 8) | (1 << 11);
+        [SerializeField] private UpdateManager updateManager;
+        // for UpdateManager
+        private int customUpdateInternalIndex;
+        private bool holding;
+
+        [SerializeField] private Transform debugIndicator;
+
+        public override void OnPickup()
+        {
+            updateManager.Register(this);
+        }
+
+        public override void OnDrop()
+        {
+            debugIndicator.gameObject.SetActive(false);
+            updateManager.Deregister(this);
+        }
+
+        public override void OnPickupUseDown()
+        {
+            holding = true;
+        }
+
+        public override void OnPickupUseUp()
+        {
+            holding = false;
+        }
+
+        public void CustomUpdate()
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(aimPoint.position, aimPoint.forward, out hit, 15f, layerMask.value) && hit.transform != null)
+            {
+                Chalkboard board = hit.transform.GetComponent<Chalkboard>();
+                if (board == null)
+                {
+                    debugIndicator.gameObject.SetActive(false);
+                    return;
+                }
+                debugIndicator.gameObject.SetActive(true);
+                debugIndicator.position = hit.point;
+                if (!holding)
+                    return;
+                Texture2D texture = (Texture2D)board.boardRenderer.material.mainTexture;
+                float x = Mathf.Abs((hit.point.x - board.bottomLeft.position.x) / (board.topRight.position.x - board.bottomLeft.position.x));
+                float y = Mathf.Abs((hit.point.y - board.bottomLeft.position.y) / (board.topRight.position.y - board.bottomLeft.position.y));
+                int xPixel = System.Math.Max(0, System.Math.Min(texture.width - 1, (int)(x * texture.width)));
+                int yPixel = System.Math.Max(0, System.Math.Min(texture.height - 1, (int)(y * texture.height)));
+                texture.SetPixel(xPixel, yPixel, Color.white);
+                texture.Apply();
+            }
+            else
+            {
+                debugIndicator.gameObject.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Chalkboard/Scripts/Chalkboard.cs
@@ -0,0 +1,16 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace JanSharp
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class Chalkboard : UdonSharpBehaviour
+    {
+        public Transform bottomLeft;
+        public Transform topRight;
+        public Renderer boardRenderer;
+        public float chalkPixelRadius;
+    }
+}
