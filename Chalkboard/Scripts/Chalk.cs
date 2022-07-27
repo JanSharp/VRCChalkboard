8d85b85f9e2de4c885900844a52f1c15dd70f0ed
JanSharp <steinmeister36@gmail.com>
2022-07-27 02:08:16 +0200

    Implement line drawing
    It's actually just drawing a 5x5 point every 2 pixels

--- Chalkboard/Scripts/Chalk.cs
+++ Chalkboard/Scripts/Chalk.cs
@@ -16,6 +16,9 @@ namespace JanSharp
         // for UpdateManager
         private int customUpdateInternalIndex;
         private bool holding;
+        private bool hasPrev;
+        private int prevX;
+        private int prevY;
 
         private Color[] colors;
 
@@ -33,6 +36,9 @@ namespace JanSharp
         {
             debugIndicator.gameObject.SetActive(false);
             updateManager.Deregister(this);
+            // because I don't trust VRC
+            holding = false;
+            hasPrev = false;
         }
 
         public override void OnPickupUseDown()
@@ -43,6 +49,7 @@ namespace JanSharp
         public override void OnPickupUseUp()
         {
             holding = false;
+            hasPrev = false;
         }
 
         public void CustomUpdate()
@@ -61,26 +68,70 @@ namespace JanSharp
                 if (!holding)
                     return;
                 Texture2D texture = (Texture2D)board.boardRenderer.material.mainTexture;
-                float x = Mathf.Abs((hit.point.x - board.bottomLeft.position.x) / (board.topRight.position.x - board.bottomLeft.position.x));
-                float y = Mathf.Abs((hit.point.y - board.bottomLeft.position.y) / (board.topRight.position.y - board.bottomLeft.position.y));
-                x = (int)(x * texture.width);
-                y = (int)(y * texture.height);
-                int xPixelBL = System.Math.Max(0, System.Math.Min(texture.width - 1, (int)(x - 2)));
-                int yPixelBL = System.Math.Max(0, System.Math.Min(texture.height - 1, (int)(y - 2)));
-                int xPixelTR = System.Math.Max(0, System.Math.Min(texture.width - 1, (int)(x + 2)));
-                int yPixelTR = System.Math.Max(0, System.Math.Min(texture.height - 1, (int)(y + 2)));
-                colors[0] = texture.GetPixel(xPixelBL, yPixelBL);
-                colors[4] = texture.GetPixel(xPixelTR, yPixelBL);
-                colors[24 - 4] = texture.GetPixel(xPixelBL, yPixelTR);
-                colors[24 - 0] = texture.GetPixel(xPixelTR, yPixelTR);
-                // FIXME: this can go out of bounds
-                texture.SetPixels(xPixelBL, yPixelBL, 5, 5, colors);
+                var width = texture.width;
+                var height = texture.height;
+                var blPos = board.bottomLeft.position;
+                var trPos = board.topRight.position;
+                int x = (int)Mathf.Clamp(Mathf.Abs((hit.point.x - blPos.x) / (trPos.x - blPos.x)) * width, 2, width - 3);
+                int y = (int)Mathf.Clamp(Mathf.Abs((hit.point.y - blPos.y) / (trPos.y - blPos.y)) * height, 2, height - 3);
+                if (hasPrev && (Mathf.Abs(x - prevX) + Mathf.Abs(y - prevY)) <= 2) // didn't draw more than 2 pixels from prev point? => ignore
+                    return;
+                DrawFromPrevTo(texture, x, y);
                 texture.Apply();
             }
             else
             {
                 debugIndicator.gameObject.SetActive(false);
             }
+        }
+
+        private void DrawFromPrevTo(Texture2D texture, int toX, int toY)
+        {
+            if (hasPrev)
+            {
+                Vector2 delta = new Vector2(toX - prevX, toY - prevY);
+                if (Mathf.Abs(delta.x) >= Mathf.Abs(delta.y)) // horizontal
+                {
+                    int stepX = System.Math.Sign(delta.x) * 2;
+                    float stepY = (delta.y / Mathf.Abs(delta.x)) * 2;
+                    float y = prevY;
+                    if (prevX < toX)
+                        for (int x = prevX + stepX; x <= toX - 1; x += stepX)
+                            DrawPoint(texture, x, Mathf.RoundToInt(y += stepY));
+                    else
+                        for (int x = prevX + stepX; x >= toX + 1; x += stepX)
+                            DrawPoint(texture, x, Mathf.RoundToInt(y += stepY));
+                }
+                else // vertical
+                {
+                    int stepY = System.Math.Sign(delta.y) * 2;
+                    float stepX = (delta.x / Mathf.Abs(delta.y)) * 2;
+                    float x = prevX;
+                    if (prevY < toY)
+                        for (int y = prevY + stepY; y <= toY - 1; y += stepY)
+                            DrawPoint(texture, Mathf.RoundToInt(x += stepX), y);
+                    else
+                        for (int y = prevY + stepY; y >= toY + 1; y += stepY)
+                            DrawPoint(texture, Mathf.RoundToInt(x += stepX), y);
+                }
+            }
+            DrawPoint(texture, toX, toY);
+            hasPrev = true;
+            prevX = toX;
+            prevY = toY;
+        }
+
+        private void DrawPoint(Texture2D texture, int x, int y)
+        {
+            int blX = x - 2;
+            int blY = y - 2;
+            int trX = x + 2;
+            int trY = y + 2;
+            colors[0] = texture.GetPixel(blX, blY);
+            colors[4] = texture.GetPixel(trX, blY);
+            colors[20] = texture.GetPixel(blX, trY);
+            colors[24] = texture.GetPixel(trX, trY);
+            texture.SetPixels(blX, blY, 5, 5, colors);
         }
     }
 }
