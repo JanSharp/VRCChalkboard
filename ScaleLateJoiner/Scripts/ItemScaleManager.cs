a5c9142c023ca0fc416503e196d483828656ea00
JanSharp <steinmeister36@gmail.com>
2023-06-11 21:05:15 +0200

    Account for server side caching when syncing

--- ScaleLateJoiner/Scripts/ItemScaleManager.cs
+++ ScaleLateJoiner/Scripts/ItemScaleManager.cs
@@ -1,4 +1,4 @@
-using UdonSharp;
+﻿using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
@@ -27,6 +27,8 @@ namespace JanSharp
         private int itemsCount;
         [UdonSynced] private float[] syncedScales;
         [UdonSynced] private ushort[] syncedItemIds;
+        private int requestedSerializationCount;
+        private bool waitingForOwnerToSendData;
 
         private void Start()
         {
@@ -75,6 +77,34 @@ namespace JanSharp
             itemsCount++;
         }
 
+        public override void OnPlayerJoined(VRCPlayerApi player)
+        {
+            if (Networking.LocalPlayer.IsOwner(this.gameObject) && VRCPlayerApi.GetPlayerCount() != 1)
+            {
+                SendCustomEventDelayedSeconds(nameof(RequestSerializationDelayed), 8f);
+                requestedSerializationCount++;
+            }
+            else
+            {
+                waitingForOwnerToSendData = true;
+            }
+        }
+
+        public override void OnOwnershipTransferred(VRCPlayerApi player)
+        {
+            if (waitingForOwnerToSendData && Networking.LocalPlayer.IsOwner(this.gameObject))
+            {
+                SendCustomEventDelayedSeconds(nameof(RequestSerializationDelayed), 8f);
+                requestedSerializationCount++;
+            }
+        }
+
+        public void RequestSerializationDelayed()
+        {
+            if ((--requestedSerializationCount) == 0)
+                RequestSerialization();
+        }
+
         public override void OnPreSerialization()
         {
             if (syncedItemIds == null || syncedItemIds.Length != itemsCount)
@@ -91,6 +121,7 @@ namespace JanSharp
 
         public override void OnDeserialization()
         {
+            waitingForOwnerToSendData = false;
             if (syncedItemIds == null) // just in case
                 return;
             int count = 16;
